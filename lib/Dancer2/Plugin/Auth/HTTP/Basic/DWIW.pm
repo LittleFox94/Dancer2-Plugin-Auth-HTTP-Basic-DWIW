4e6095bea661c2436115255020fc4f542ff5b609
Moritz Grosch (LittleFox) <littlefox@lf-net.org>
2016-04-23 01:49:16 +0200

    removed duplicate pod-sections, some code cleanup

--- lib/Dancer2/Plugin/Auth/HTTP/Basic/DWIW.pm
+++ lib/Dancer2/Plugin/Auth/HTTP/Basic/DWIW.pm
@@ -12,9 +12,10 @@ our $CHECK_LOGIN_HANDLER = undef;
 register http_basic_auth => sub {
     my ($dsl, $stuff, $sub, @other_stuff) = @_;
 
-    my $realm = plugin_setting->{'realm'} || 'Please login';
+    my $realm = plugin_setting->{'realm'} // 'Please login';
 
     return sub {
+        local $@ = undef;
         eval {
             my $header = $dsl->app->request->header('Authorization') || die \401;
 
@@ -58,12 +59,13 @@ register http_basic_auth_login => sub {
 
     my @auth_header = split(' ', $dsl->app->request->header('Authorization'));
     my $auth_string = $auth_header[1];
-
-    my @auth_parts = split(':', decode_base64($auth_string));
+    my @auth_parts  = split(':', decode_base64($auth_string));
 
     return @auth_parts;
-    },
-    {is_global => 0};
+},
+{
+    is_global => 0
+};
 
 register http_basic_auth_set_check_handler => sub {
     my ($dsl, $handler) = @_;
@@ -125,10 +127,6 @@ Defaults to "Please login".
 
 This is my first perl module published on CPAN. Please don't hurt me when it is bad and feel free to make suggestions or to fork it on GitHub.
 
-=head1 AUTHOR
-
-Moritz Grosch (LittleFox), C<< <littlefox at fsfe.org> >>
-
 =head1 BUGS
 
 Please report any bugs or feature requests to C<littlefox at fsfe.org>, or through
@@ -137,48 +135,8 @@ automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
-You can find documentation for this module with the perldoc command.
+After installation you can find documentation for this module with the perldoc command:
 
     perldoc Dancer2::Plugin::Auth::HTTP::Basic::DWIW
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2015 Moritz Grosch (LittleFox).
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
